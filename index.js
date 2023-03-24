cda225cec8b69e2dea69fa5a98f500706090dd92
Steven <03stevennguyen@gmail.com>
2023-03-24 10:10:27 -0400

    Updated index.js and moved .html/css to public

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express()
+app.use(express.static('public'));
 const port = 3000
 const stripe = require('stripe')('sk_test_51MiU4xLGe3DYUFyZAHJjWk6FWztmtnjTwjNvzEGQpZCWqyqDbe5Lr3Iag442Nu0mzdEwiRvYFjw5vVhCFfFPG55M00DXkYvohM')
 const pageURL =
     'https://www.newegg.com/intel-core-i5-12600k-core-i5-12th-gen/p/N82E16819118347?Description=processors&cm_re=processors-_-19-118-347-_-Product';
 
 var mysql = require('mysql');
-const YOUR_DOMAIN = 'http://localhost/:$%7Bport%7D';
+const YOUR_DOMAIN = 'http://localhost:3000';
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -15,40 +16,52 @@ var con = mysql.createConnection({
     password: "password",
     database: "parts"
 });
+
+var values = []
+var sql = ""
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile('public/home.html', { root: __dirname })
 })
 
 app.get('/checkout', (req, res) => {
-    res.sendFile('checkout.html', { root: dirname })
+    res.sendFile('public/checkout.html', { root: __dirname })
 })
 
 app.get('/success', (req, res) => {
-    res.sendFile('success.html', { root: dirname })
+    res.sendFile('public/success.html', { root: __dirname })
 })
 
-app.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          price: '{{PRICE_ID}}',
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      success_url: `${YOUR_DOMAIN}/success.html`,
-      cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+app.post('/insert-item', (req, res) => {
+    console.log(sql);
+    con.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
     });
-  
-    res.redirect(303, session.url);
-  });
-  
+    res.redirect(303,YOUR_DOMAIN+'/'); 
+})
+
+app.post('/create-checkout-session', async(req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: 'price_1Mp0n0LGe3DYUFyZsf1jnQNW',
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}/success.html`,
+        cancel_url: `${YOUR_DOMAIN}/`,
+      });
+    
+      res.redirect(303, session.url); 
+     });
+
 app.listen(port, () => {
     console.log('Example app listening at', port)
 })
@@ -62,15 +75,15 @@ const webscraping = async (pageURL) => {
     let dataObj = {};
     try {
         await page.goto(pageURL);
-        //work on this
-        //use $i to iterate through tables
-        let attributeList = [];
+        //let attributeList = [];
         const scrappedItems = await page.evaluate(() => {
-            //what the hell is happening here
-            keys = Array.from(document.querySelectorAll('#product-details > div.tab-panes > div:nth-child(2) > table tr th'));
-            values = Array.from(document.querySelectorAll('#product-details > div.tab-panes > div:nth-child(2) > table tr td'));
-            keys = keys.map(th => th.innerText);
-            values = values.map(td => td.innerText);
+            //getting tables
+            let keys = Array.from(document.querySelectorAll('#product-details > div.tab-panes > div:nth-child(2) > table tr th'));
+            let values = Array.from(document.querySelectorAll('#product-details > div.tab-panes > div:nth-child(2) > table tr td'));
+            keys = keys.map(th => th.innerText.trim());
+            values = values.map(td => td.innerText.trim());
+            keys = keys.map(x => Array.from(new Set(x.split(' '))).toString().split(',').join(' '));
+            //values = values.map(x => Array.from(new Set(x.split(' '))).toString().split(',').join(' '));
             const merged = keys.reduce((obj, key, index) => ({ ...obj, [key]: values[index] }), {});
             return merged
         });
@@ -91,13 +104,13 @@ let cpu = new Promise((res) => {
     // prod_name, cores, socket, compatibility, mem_type
 });
 cpu.then((result) => {
-    var values = [
-        result.scrappedItems['Name '],
-        result.scrappedItems['# of Cores # of Cores'],
-        result.scrappedItems['CPU Socket Type CPU Socket Type'],
-        result.scrappedItems['64-Bit Support 64-Bit Support'],
-        result.scrappedItems['Memory Types ']];
-    var sql = "INSERT INTO cpu (prod_name, cores, socket, compatibility, mem_type) VALUES (?)";
+    values = [
+        result.scrappedItems['Name'],
+        result.scrappedItems['# of Cores'],
+        result.scrappedItems['CPU Socket Type'],
+        result.scrappedItems['64-Bit Support'],
+        result.scrappedItems['Memory Types']];
+    sql = "INSERT INTO cpu (prod_name, cores, socket, compatibility, mem_type) VALUES (?)";
     con.query(sql, [values], function (err, result) {
         if (err) throw err;
         console.log("Number of records inserted: " + result.affectedRows);
