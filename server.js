da74f0157591ff30cc2931af35bdc9713eaebdd7
eventswastaken <03stevennguyen@gmail.com>
2023-04-26 16:36:18 -0400

    Lots of moving around and deleting .html files Added links to various buttons Ran code through prettier

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const crypto = require('crypto');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const crypto = require("crypto");
 const port = 3000;
-const uuidv4 = require('uuid').v4;
-const inventory = require('./lib/inventory');
-const review = require('./lib/review');
-const account = require('./lib/account');
-const chat_server = require('./lib/chat/chat_server');
-const user = require('./lib/user');
-const cart = require('./lib/cart');
-
+const uuidv4 = require("uuid").v4;
+const inventory = require("./lib/inventory");
+const review = require("./lib/review");
+const account = require("./lib/account");
+const chat_server = require("./lib/chat/chat_server");
+const user = require("./lib/user");
+const cart = require("./lib/cart");
 
 //connect this node.js server to the mysql server...
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'admin',
-    database: 'rbuild'
+  host: "127.0.0.1",
+  user: "root",
+  password: "admin",
+  database: "rbuild",
 });
 
 connection.connect();
@@ -28,26 +27,30 @@ connection.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(crypto.randomBytes(64).toString()));
 app.use(express.json());
-app.use(cors({credentials: true, origin: 'http://localhost'}));
-app.use(express.static('public', {
-  setHeaders: (res, path) => {
+app.use(cors({ credentials: true, origin: "http://localhost" }));
+app.use(
+  express.static("public", {
+    setHeaders: (res, path) => {
       // import mime-types package
-      const mime = require('mime-types');
+      const mime = require("mime-types");
       const type = mime.lookup(path);
-      // explicitly set the Content-Type header for each response 
-      res.setHeader('Content-Type', type);
-  }
-  }));
+      // explicitly set the Content-Type header for each response
+      res.setHeader("Content-Type", type);
+    },
+  })
+);
 
-app.use('/socket.io', express.static(__dirname + '/node_modules/socket.io/client-dist'));
-
+app.use(
+  "/socket.io",
+  express.static(__dirname + "/node_modules/socket.io/client-dist")
+);
 
 inventory(app, connection);
 review(app, connection);
 account(app, connection);
 chat_server(app, connection);
 user(app, connection);
-cart(app,connection);
+cart(app, connection);
 
 app.use((req, res, next) => {
   // check if the user already has a UUID
@@ -56,14 +59,13 @@ app.use((req, res, next) => {
     // generate a new UUID for the user
     userid = uuidv4();
     // set the userId cookie
-    res.cookie('userId', userid);
+    res.cookie("userId", userid);
   }
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send(`Your user ID is ${req.cookies.userId}`);
-  res.sendFile(path.join(__dirname, 'public','index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
